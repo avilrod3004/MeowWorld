ef50939331f416dcb60a4591e64bbd63c71fbfd2
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-12 22:01:32 +0100

    implementado: borrar post corregir redireccion

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -7,6 +7,12 @@
             <p>@{{ this.comment.user.username }}</p>
             <p>{{ this.comment.text }}</p>
             <p>{{ formatData(this.comment.created_at) }}</p>
+            <button @click="showModalDeleteComment = true">Borrar</button>
+            <Modal :is-open="showModalDeleteComment">
+                <p>¿Quiere eliminar este comentario?</p>
+                <button @click="showModalDeleteComment = false">Cancelar</button>
+                <button @click="deleteComment">Borrar comentario</button>
+            </Modal>
         </div>
 
         <hr>
@@ -15,17 +21,43 @@
 
 <script>
 import formatData from "../helpers/formatData.js";
+import api from "../helpers/api.js";
+import Modal from "../modals/Modal.vue";
 
 export default {
     name: "Comment",
-    methods: {formatData},
+    components: {Modal},
 
     props: {
         comment: {
             type: Object,
             required: true
         }
-    }
+    },
+
+    data() {
+        return {
+            showModalDeleteComment: false,
+        }
+    },
+
+    methods: {
+        formatData,
+
+        async deleteComment() {
+            try {
+                const responseDeleteComment = await api.delete(`/comments/${this.comment.id}`);
+                console.log(this.comment.post.id);
+
+                if (responseDeleteComment.status === 200) {
+                    this.$router.back() // :(
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
 }
 </script>
 
