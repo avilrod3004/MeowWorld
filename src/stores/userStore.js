38f221e848bd8c4c9c762accd4f00be444baf1c5
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-12 12:54:26 +0100

    imagenes de perfil por defecto

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -4,9 +4,28 @@ export const useUserStore = defineStore("user", {
     state: () => ({
         user: null,
     }),
+
+    getters: {
+        getUser: (state) => state.user,
+    },
+
     actions: {
         setUser(user) {
-            this.user = user
+            this.user = user;
         },
+        logout() {
+            this.user = null;
+            // localStorage.removeItem("user");
+        }
+    },
+
+    persist: {
+        enabled: true,
+        strategies: [
+            {
+                key: "user",
+                storage: localStorage, // Solo guarda los datos del usuario
+            }
+        ]
     }
 })
