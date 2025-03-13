a1de2873934fbcbfea243bbf31ebceb36e72da64
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-13 22:36:51 +0100

    mostrar spinner al cargar los resultados de las busquedas y mostrar errores

---
+++ src/components/ListProfiles.vue
@@ -0,0 +1,93 @@
+<template>
+    <div v-if="type === 'users'">
+        <ul v-if="profiles && profiles.length > 0" class="listado">
+            <li v-for="user in profiles" :key="user.id" @click="gotToUserProfile(user.id)" class="cuenta">
+                <img :src="user?.img_profile || defaultImage" alt="" class="img__profile">
+                <div class="cuenta__nombres">
+                    <p class="nombres__nombre">{{ user.name }}</p>
+                    <p>@{{ user.username }}</p>
+                </div>
+            </li>
+        </ul>
+        <p v-if="profiles && profiles.length === 0" class="sin_resultado">No hay usuarios que tengan ese nombre.</p>
+    </div>
+
+    <div v-if="type === 'cats'" class="listado">
+        <ul v-if="profiles && profiles.length > 0" class="listado">
+            <li v-for="cat in profiles" :key="cat.id" @click="goToCatProfile(cat.id)" class="cuenta">
+                <img :src="cat?.image || defaultImage" alt="" class="img__profile">
+                <div class="cuenta__nombres">
+                    <p class="nombres__nombre">{{ cat.name }}</p>
+                    <p>@{{ cat.owner.username }}</p>
+                </div>
+            </li>
+        </ul>
+        <p v-if="profiles && profiles.length === 0" class="sin_resultado">No hay gatos que tengan ese nombre.</p>
+    </div>
+</template>
+
+<script>
+import defaultImg from '../assets/default_img_profile.png';
+
+export default {
+    name: "ListProfiles",
+
+    data() {
+        return {
+            defaultImage: defaultImg,
+        }
+    },
+
+    props: {
+        profiles: {
+            type: Array,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        }
+    },
+
+    methods: {
+        goToCatProfile(catId) {
+            this.$router.push({ path: `/cat/${catId}` });
+        },
+
+        gotToUserProfile(userId) {
+            this.$router.push({ name: 'OtherUserProfile', params: { id: userId } });
+        }
+    }
+}
+</script>
+
+<style scoped>
+.cuenta {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+
+    cursor: pointer;
+
+    padding: 1rem 0.5rem;
+    border-bottom: 0.1rem solid #ccc;
+}
+
+.img__profile {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+.cuenta__nombres {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+}
+
+.nombres__nombre {
+    font-family: "DynaPuff", system-ui;
+    font-size: 1.25rem;
+}
+</style>
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,42 +1,31 @@
 <template>
-    <h1>Busqueda</h1>
+    <h1 class="titulo">Busqueda</h1>
 
     <Form @submit="searchQuery" :validation-schema="schema" class="formulario">
-        <Field as="select" name="category" class="form__input" :v-model="selectedCategory">
+        <Field aria-label="Tipo de cuenta" as="select" name="category" class="form__input" :v-model="selectedCategory">
             <option value="" disabled selected>Categoría</option>
             <option value="users">Usuarios</option>
             <option value="cats">Gatos</option>
-            <option value="posts">Posts</option>
         </Field>
         <ErrorMessage name="category" class="form__error"/>
 
-        <Field name="query" class="form__input" placeholder="Palabra clave" />
+        <Field aria-label="Palabra clave" name="query" class="form__input" placeholder="Palabra clave" />
         <ErrorMessage name="email" class="form__error"/>
 
         <button type="submit" class="button__primary">Aplicar</button>
+
+        <ErrorsList :errors-server="errors" />
     </Form>
 
-    <section>
-        <h1>Resultados</h1>
-        <p v-if="resultsCount">{{ resultsCount }} coincidencia<span v-if="resultsCount > 1">s</span></p>
+    <section class="resultados">
+        <h1 class="titulo">Resultados</h1>
 
-        <ul v-if="users && users.length > 0">
-            <li v-for="user in users" :key="user.id" @click="gotToUserProfile(user.id)" class="item">
-                <img :src="user.img_profile" alt="">
-                <p>{{ user.name }}</p>
-                <p>{{ user.username }}</p>
-            </li>
-        </ul>
-        <p v-if="users && users.length === 0">No hay usuarios que tengan ese nombre</p>
+        <Spinner v-if="loading"/>
+        <div v-else class="resultados__listados">
+            <p v-if="resultsCount">{{ resultsCount }} coincidencia<span v-if="resultsCount > 1">s</span></p>
 
-        <ul v-if="cats && cats.length > 0">
-            <li v-for="cat in cats" :key="cat.id" @click="goToCatProfile(cat.id)" class="item">
-                <img :src="cat.image" alt="">
-                <p>{{ cat.name }}</p>
-                <p>{{ cat.owner.username }}</p>
-            </li>
-        </ul>
-        <p v-if="cats && cats.length === 0">No hay gatos que tengan ese nombre</p>
+            <ListProfiles :profiles="this.profiles" :type="this.type" />
+        </div>
     </section>
 </template>
 
@@ -44,11 +33,17 @@
 import * as yup from 'yup';
 import {Form, Field, ErrorMessage} from "vee-validate";
 import api from "../helpers/api.js";
+import Spinner from "../components/Spinner.vue";
+import ErrorsList from "../components/ErrorsList.vue";
+import ListProfiles from "../components/ListProfiles.vue";
 
 export default {
     name: "Search",
 
     components: {
+        ListProfiles,
+        ErrorsList,
+        Spinner,
         Form,
         Field,
         ErrorMessage,
@@ -57,75 +52,70 @@ export default {
     data() {
         return {
             errors: [],
+            loading: false,
             selectedCategory: '',
             schema: yup.object().shape({
                 query: yup.string(),
                 category: yup.string().required("Selecciona una categoría"),
             }),
-            users: null,
-            cats: null,
-            posts: null,
             resultsCount: null,
+            profiles: null,
+            type: null,
         }
     },
 
     methods: {
         async searchQuery(values) {
+            this.loading = true;
             this.errors = [];
 
-            // Obtenemos la query y la categoría seleccionada
             const { query, category } = values;
 
             try {
-                // Dependiendo de la categoría seleccionada, hacemos la llamada API correspondiente
                 let response;
                 if (category === "users") {
                     response = await api.get(`/users/filter?query=${query}`);
 
                     if (response.status === 200) {
-                        this.users = null;
-                        this.cats = null;
-                        this.posts = null;
+                        this.profiles = null;
+                        this.type = null;
 
-                        this.users = response.data.data;
+                        this.profiles = response.data.data;
                         this.resultsCount = response.data.meta.total;
+                        this.type = "users"
                     }
                 } else if (category === "cats") {
                     response = await api.get(`/cats/filter?query=${query}`);
 
                     if (response.status === 200) {
-                        this.users = null;
-                        this.cats = null;
-                        this.posts = null;
+                        this.profiles = null;
+                        this.type = null;
 
-                        this.cats = response.data.data;
+                        this.profiles = response.data.data;
                         this.resultsCount = response.data.meta.total;
+                        this.type = "cats"
                     }
                 } else if (category === "posts") {
                     response = await api.get(`/posts?search=${query}`);
                 }
-
-                // Aquí puedes manejar la respuesta de la API, por ejemplo, actualizar el estado o mostrar resultados
-                console.log(response.data.data);
+                this.loading = false;
             } catch (error) {
-                console.log("Error en la búsqueda:", error);
-                this.errors.push("Hubo un error al realizar la búsqueda.");
+                this.errors = error.response?.data.errors || ["Ha ocurrido un error al realizar la búsqueda. Vuelva a intentarlo más tarde."];
+
+                this.loading = false;
             }
         },
-
-        goToCatProfile(catId) {
-            this.$router.push({ path: `/cat/${catId}` });
-        },
-
-        gotToUserProfile(userId) {
-            this.$router.push({ name: 'OtherUserProfile', params: { id: userId } });
-        }
     }
 
 }
 </script>
 
 <style scoped>
+.titulo {
+    font-family: "DynaPuff", system-ui;
+    font-size: 2rem;
+}
+
 .formulario {
     display: flex;
     flex-direction: column;
@@ -135,7 +125,17 @@ export default {
     padding: 1rem 0
 }
 
-.item {
-    cursor: pointer;
+.resultados {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    padding-top: 1rem;
+}
+
+.resultados__listados {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
 }
 </style>
