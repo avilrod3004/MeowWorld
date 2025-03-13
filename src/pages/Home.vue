61e3b5a35f471eb07832dc91c3157e489550bf6b
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-13 18:33:12 +0100

    mensajes de error

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,21 +3,21 @@
         <Post v-for="post in posts" :key="post.id" :post="post" :all-info="false"/>
     </div>
 
-    <p v-if="!posts">Cargando....</p>
     <p v-if="posts && posts.length === 0" class="sin_resultado">No hay posts por aquí... 🐾 ¿Están todos los gatitos dormidos?</p>
 
-    <ul v-if="errorsServer">
-        <li v-for="error in errorsServer" class="form__error">{{ error }}</li>
-    </ul>
+    <p v-if="!posts">Cargando....</p>
+    <ErrorsList :errorsServer="errorsServer"/>
 </template>
 
 <script>
 import Post from "../components/Post.vue";
 import api from "../helpers/api.js";
+import ErrorsList from "../components/ErrorsList.vue";
+import Spinner from "../components/Spinner.vue";
 
 export default {
     name: "Home",
-    components: {Post},
+    components: {Spinner, ErrorsList, Post},
 
     data() {
         return {
@@ -32,7 +32,7 @@ export default {
                 const responsePosts = await api.get('posts')
                 this.posts = responsePosts.data.data;
             } catch (error) {
-                this.errorsServer = error.response?.data.errors || ["[ERROR]: Ha ocurrido un error inesperado. Vuelva a interntarlo más tarde."];
+                this.errorsServer = error.response?.data.errors || ["Ha ocurrido un error inesperado. Vuelva a interntarlo más tarde."];
             }
         }
     },
