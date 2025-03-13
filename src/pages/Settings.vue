b3cbac8bd586e2724c5d5d9ac49388027fe7c757
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-13 13:41:47 +0100

    implementacion cambiar el email y la contraseña del usuario

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -5,14 +5,47 @@
         <div class="settings__group">
             <h2 class="group__title">Gestion usuario</h2>
             <ul class="group__list">
-                <li class="list__option">
+                <li class="list__option" @click="showModalEditEmail = true">
                     <font-awesome-icon icon="envelope" class="icono"/>
                     Cambiar email
                 </li>
-                <li class="list__option">
+
+                <Modal :is-open="showModalEditEmail">
+                    <Form @submit="updateEmail" :validation-schema="schemaEmail">
+                        <label for="email">Escribe el nuevo email:</label>
+                        <Field name="email" class="form__input" placeholder="Nuevo email"/>
+                        <ErrorMessage name="email" class="form__error" />
+
+                        <div>
+                            <button class="button__cancel" @click="showModalEditEmail = false">Cancelar</button>
+                            <button class="button__confirm" type="submit">Actualizar</button>
+                        </div>
+                    </Form>
+                </Modal>
+
+                <li class="list__option" @click="showModalEditPassword = true">
                     <font-awesome-icon icon="key" class="icono"/>
                     Cambiar contraseña
                 </li>
+
+                <Modal :is-open="showModalEditPassword">
+                    <Form @submit="updatePassword" :validation-schema="schemaPassword">
+                        <label for="password">Escribe la nueva contraseña:</label>
+                        <Field name="password" class="form__input" placeholder="Nueva contraseña"/>
+                        <ErrorMessage name="password" class="form__error" />
+
+                        <label for="password_confirmation">Repite la nueva contraseña:</label>
+                        <Field name="password_confirmation" class="form__input" placeholder="Repite la constraseña"/>
+                        <ErrorMessage name="password_confirmation" class="form__error" />
+
+                        <div>
+                            <button class="button__cancel" @click="showModalEditPassword = false">Cancelar</button>
+                            <button class="button__confirm" type="submit">Actualizar</button>
+                        </div>
+                    </Form>
+                </Modal>
+
+
                 <li @click="logout" class="list__option">
                     <font-awesome-icon icon="power-off" class="icono"/>
                     Cerrar sesión
@@ -63,17 +96,52 @@ import { faCircleHalfStroke, faPowerOff, faKey, faEnvelope, faCat, faTriangleExc
 import api from "../helpers/api.js";
 import {mapState} from "pinia";
 import {useUserStore} from "../stores/userStore.js";
+import Modal from "../modals/Modal.vue";
+import * as yup from 'yup';
+import {Form, Field, ErrorMessage} from "vee-validate";
 
 library.add(faCircleHalfStroke, faPowerOff, faKey, faEnvelope, faCat, faTriangleExclamation, faCode, faInfoCircle);
 
 export default {
     name: "Configuration",
-    components: {FontAwesomeIcon},
+
+    components: {
+        Modal,
+        FontAwesomeIcon,
+        Form,
+        Field,
+        ErrorMessage,
+    },
 
     data() {
         return {
             showModalEditEmail: null,
             showModalEditPassword: null,
+            schemaEmail: yup.object().shape({
+                email: yup
+                    .string()
+                    .trim()
+                    .required('Ingrese el email')
+                    .email('El formato del email no es válido')
+                    .matches(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm, 'El formato del email no es válido'),
+
+            }),
+            schemaPassword: yup.object().shape({
+                password: yup
+                    .string()
+                    .trim()
+                    .required("Ingrese la contraseña")
+                    .matches(
+                        /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
+                        "La contraseña no cumple los requisitos. Debe tener una longitud mínima de 8 caracteres y contener una letra en mayúscula, un número y un caracter especial."
+                    ),
+                password_confirmation: yup
+                    .string()
+                    .trim()
+                    .oneOf([yup.ref("password"), null], "Las contraseñas deben coincidir")
+                    .required("Por favor, repita la contraseña")
+
+            })
         }
     },
 
@@ -87,6 +155,43 @@ export default {
             const currentTheme = body.getAttribute("data-theme");
             const newTheme = currentTheme === "dark" ? "light" : "dark";
             body.setAttribute("data-theme", newTheme);
+        },
+
+        async updateEmail(values) {
+            try {
+                const responseUpdate = await api.post(`users/${useUserStore().getUser.id}/credentials`,
+                    {
+                        email: values.email,
+                    }
+                );
+
+                if (responseUpdate.status === 200) {
+                    console.log("email actualizado");
+                    this.showModalEditEmail = false;
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        },
+
+        async updatePassword(values) {
+            try {
+                const responseUpdate = await api.post(`users/${useUserStore().getUser.id}/credentials`,
+                    {
+                        password: values.password,
+                        password_confirmation: values.password_confirmation
+                    }
+                );
+
+                if (responseUpdate.status === 200) {
+                    console.log("contraseña actualizada actualizado");
+                    useUserStore().logout();
+                    localStorage.removeItem('token');
+                    window.location.href = '/';
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
         },
 
         async logout() {
