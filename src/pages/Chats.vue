9e3a43352282e2c95947489b388999d5f6cb3993
avilrod3004 <avilrod3004@g.educaand.es>
2025-03-14 13:09:33 +0100

    futuras implementaciones

--- src/pages/Chats.vue
+++ src/pages/Chats.vue
@@ -1,13 +1,25 @@
+<template>
+    <div class="mensaje">
+        <h1 class="titulo">Chats</h1>
+        <p>Esta funcionalidad estará disponible próximamente. Estamos trabajando para ofrecerte la mejor experiencia de comunicación.</p>
+    </div>
+</template>
+
 <script>
 export default {
     name: "Chats"
 }
 </script>
 
-<template>
-<h1>Chats</h1>
-</template>
-
 <style scoped>
+.mensaje {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
 
+.titulo {
+    font-family: "DynaPuff", system-ui;
+    font-size: 2rem;
+}
 </style>
--- src/pages/Notifications.vue
+++ src/pages/Notifications.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Notificaciones</h1>
+    <div class="mensaje">
+        <h1 class="titulo">Notificaciones</h1>
+        <p>Aquí podrás ver todas tus notificaciones en tiempo real. Esta sección estará disponible en una futura actualización. ¡Gracias por tu paciencia!</p>
+    </div>
 </template>
 
 <script>
@@ -9,5 +12,14 @@ export default {
 </script>
 
 <style scoped>
+.mensaje {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
 
+.titulo {
+    font-family: "DynaPuff", system-ui;
+    font-size: 2rem;
+}
 </style>
